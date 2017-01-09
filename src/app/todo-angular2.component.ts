a71b610aacb22dc783d6960f8e681a46923b6d1a
Darlan Damasio <damasio34@gmail.com>
2017-01-09 17:25:59 -0200

    Implementando o componente

--- src/app/todo-angular2.component.ts
+++ src/app/todo-angular2.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+import { TodoDataService } from './todo-data.service';
 
 @Component({
-  moduleId: module.id,
-  selector: 'todo-angular2-app',
-  templateUrl: 'todo-angular2.component.html',
-  styleUrls: ['todo-angular2.component.css']
+    moduleId: module.id,
+    selector: 'todo-angular2-app',
+    templateUrl: 'todo-angular2.component.html',
+    styleUrls: ['todo-angular2.component.css'],
+    providers: [TodoDataService]
 })
 export class TodoAngular2AppComponent {
-  title = 'todo-angular2 works!';
+
+    newTodo: Todo = new Todo();
+
+    constructor(private todoDataService: TodoDataService) {};
+
+    addTodo() {
+        this.todoDataService.addTodo(this.newTodo);
+        this.newTodo = new Todo();
+    }
+
+    toggleTodoComplete(todo) {
+        this.todoDataService.toggleTodoComplete(todo);
+    }
+
+    removeTodo(todo) {
+        this.todoDataService.deleteTodoById(todo.id);
+    }
+
+    get todos() {
+        return this.todoDataService.getAllTodos();
+    }    
 }
